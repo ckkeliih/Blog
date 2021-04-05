ed43ec734cfc6f61cc281f25185a93cd40c28806
ckkeliih <>
2021-04-05 08:28:56 +0000

    (1)Added a weather page (2)Created submit form for city (3)Displayed results

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const https = require("https");
 const homeStartingContent = "Introductory content for my blog. This is the starting page for my daily journal.";
 const aboutContent = "This page describes what the blog is all about and some information about myself.";
 const contactContent = "Brief information about how to contact me via social media.";
+const weatherContent = "Please submit a city name for weather details.";
+
+let weatherInfo = [];
 
 const app = express();
 
@@ -59,7 +62,7 @@ app.get("/posts/:postName", function(req, res){
     const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
-      res.render("post", {
+      res.render("", {
         title: post.title,
         content: post.content
       });
@@ -70,11 +73,52 @@ app.get("/posts/:postName", function(req, res){
 
 // need to write out an app.get function that open a route /weather Need an EJS view called weather.ejs that displays one text field to input city name
 //This EJS view will input a city name from user
-// Then need to write out an app.get function that will use the city name to query the Weather API to retrieve basic weather information - temperature, description and humidity
+// Then need to write out an app.get function that will use the city name to query the Weather API to retrieve City Name, Weather Image, Temperature in F, Description, Humidity and Wind Direction
 // The display of the weather information must be saved to an array and then the results of the array must be pushed to the /weather EJS view to display
 // The /weather route and page created by weather.ejs page should allow for the input of the city name, and the display of the weather for the city - city name, weather icon image, temperature in F, description, humidity, wind direction
+
+app.get("/weather", function(req, res) {
+  res.render("weather", {weatherContent:weatherContent, weatherInfo: weatherInfo});
+});
+
+app.post("/weather", function(req, res) {
+  const input = req.body.cityInput;
+  const apiKey = "12f0882419161c36a8f0837248f8440f";
+  const units = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + apiKey + "&units=" + units;
+
+  console.log(input);
+
+  https.get(url, function(response) {
+
+    response.on("data", function(data) {
+     const weatherData = JSON.parse(data);
+      const city = weatherData.name;
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const humidity = weatherData.main.humidity;
+      const direction = weatherData.wind.deg;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      const weather = {
+        city: city,
+        temp: temp,
+        description: description,
+        humidity: humidity,
+        direction: direction,
+        image: imageURL
+        };
+      
+      weatherInfo.push(weather);
+      res.redirect("/weather");
+    });
+  });
+      
+});
 
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
